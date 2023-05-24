54857085a7882cc2909b069bd44ba41f392fc3d4
Belard7 <134239532+Belard7@users.noreply.github.com>
2023-05-24 17:25:48 -0600

    eo

--- src/main/java/com/example/el_mejorcito_login/RegistroActivity.kt
+++ src/main/java/com/example/el_mejorcito_login/RegistroActivity.kt
@@ -1,10 +1,12 @@
 package com.example.el_mejorcito_login
 
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
 
 class RegistroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,12 +28,62 @@ class RegistroActivity : AppCompatActivity() {
 
 
             if(userName.isEmpty()){
+                showAlertDialog("Campo requerido", "Por favor, ingrese su nombre de usuario.")
+                return@setOnClickListener
+            }
+
+            if (!userName.matches("[a-zA-Z]+".toRegex())) {
+                showAlertDialog("Formato incorrecto", "El campo de nombre solo debe contener letras.")
+                return@setOnClickListener
+            }
+
+            if (lastName.isEmpty()){
+                showAlertDialog("Campo requerido","por favor , ingrese su apellido"
+                )
+                return@setOnClickListener
+            }
+
+            if (!lastName.matches("[a-zA-Z]+".toRegex())) {
+                showAlertDialog("Formato incorrecto", "El campo de nombre solo debe contener letras.")
+                return@setOnClickListener
+            }
+
+            if (password.isEmpty()){
+                showAlertDialog("campo requerido", "por favor, ingrese una contraseña")
+                return@setOnClickListener
+            }
+
+            if (passAgain.isEmpty()) {
+                showAlertDialog("campo requerido", "por favor, ingrese nuevamete su contraseña")
+                return@setOnClickListener
+            }
+
+            else
+            {
+                val intent = Intent(this,LoginMain2Activity::class.java)
+                startActivity(intent)
             }
 
 
-            val intent = Intent(this,LoginMain2Activity::class.java)
-            startActivity(intent)
+
 
         }
+
+
     }
+
+    private fun showAlertDialog(tittle: String, message: String) {
+        val alertDialogBuilder = AlertDialog.Builder(this)
+        alertDialogBuilder.setTitle(title)
+        alertDialogBuilder.setMessage(message)
+        alertDialogBuilder.setPositiveButton("Aceptar", DialogInterface.OnClickListener { dialog, _ ->
+            dialog.dismiss()
+        })
+
+        val alertDialog = alertDialogBuilder.create()
+        alertDialog.show()
+
+    }
+
+
 }
